a2f6ed82d4a523f8b3f04847472a4df14987001b
Ana Beatriz Oliveira de Macedo <anabeatrizmacedo241@gmail.com>
2021-07-12 13:17:38 -0300

    Gabarito caderno 1

---
+++ Caderno-01-gabarito.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 12 13:09:59 2021
+
+@author: anabeatrizmacedo
+""" 
+nota1 = 8
+nota2 = 6
+média = (8+6)/2
+print(média)
+
+salário = 5000
+aumento = 15
+novo_salário = 5000 + (5000*15/100)
+print(novo_salário)
+
+a = 'Estou aprendendo'
+b = ' Python!'
+print(a+b)
+
+ano_atual = 2021
+ano_nascimento = 2003
+idade = ano_atual - ano_nascimento
+print(idade)
+
+
